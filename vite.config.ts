472a3416a3f91b0950648744431c85c5bb7d6285
Minsu Kim <alstn2468_@naver.com>
2022-09-05 02:54:35 +0900

    refac: 함수, 데이터로 분리

--- vite.config.ts
+++ vite.config.ts
@@ -1,45 +1,37 @@
-import { defineConfig } from 'vite';
+import { defineConfig, HtmlTagDescriptor } from 'vite';
 import { createHtmlPlugin } from 'vite-plugin-html';
+
+const metaDatas = [
+  {
+    property: 'og:type',
+    content: 'website',
+  },
+  {
+    property: 'og:image',
+    content: '/images/og.png',
+  },
+  {
+    property: 'og:title',
+    content: '프론트엔드에서 함수형을 추구하면 안되는 걸까?',
+  },
+  {
+    property: 'og:description',
+    content: '함수형 프로그래밍에 대한 이론과 프론트엔드 개발을 하면서 함수형 프로그래밍을 적용한 사례를 소개합니다.',
+  },
+];
+
+const generateMetaTag = (meta: typeof metaDatas[number]): HtmlTagDescriptor => ({
+  injectTo: 'head-prepend',
+  tag: 'meta',
+  attrs: { ...meta },
+});
 
 export default defineConfig({
   plugins: [
     createHtmlPlugin({
       minify: true,
       inject: {
-        tags: [
-          {
-            injectTo: 'head-prepend',
-            tag: 'meta',
-            attrs: {
-              property: 'og:type',
-              content: 'website',
-            },
-          },
-          {
-            injectTo: 'head-prepend',
-            tag: 'meta',
-            attrs: {
-              property: 'og:image',
-              content: '/images/og.png',
-            },
-          },
-          {
-            injectTo: 'head-prepend',
-            tag: 'meta',
-            attrs: {
-              property: 'og:title',
-              content: '프론트엔드에서 함수형을 추구하면 안되는 걸까?',
-            },
-          },
-          {
-            injectTo: 'head-prepend',
-            tag: 'meta',
-            attrs: {
-              property: 'og:description',
-              content: '함수형 프로그래밍에 대한 이론과 프론트엔드 개발을 하면서 함수형 프로그래밍을 적용한 사례를 소개합니다.',
-            },
-          },
-        ],
+        tags: metaDatas.map(generateMetaTag),
       },
     }),
   ],
